9c57aad253eeb93922eeceee0acbb14aefbfa2ad
Dyego Magno <dyegowolf@gmail.com>
2023-08-29 14:57:29 -0300

    Add JavaScript functions and buttons

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function welcome(){
+    var nome = prompt("Informe o seu nome: ");
+    var h4 = document.getElementById("id1");
+    h4.innerText = "Sejam bem-vindo, " + nome;
+}
+
+function count(){
+    var p1 = document.getElementById("p1");
+    p1.innerText = parseInt(p1.innerText) + 1;
+    console.log("aaaaaa");
+}
